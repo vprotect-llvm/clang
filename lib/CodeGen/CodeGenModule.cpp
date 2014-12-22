ab62497c08d58b77c268d2e71c2d7d40f36bb2b3
Damien Engels <engelsdamien@gmail.com>
2014-12-22 23:42:12 +0100

    Added error message for multiple inheritance

--- lib/CodeGen/CodeGenModule.cpp
+++ lib/CodeGen/CodeGenModule.cpp
@@ -3350,6 +3350,9 @@ void CodeGenModule::EmitCPSVirtualMetadata(){
 
         /* Add metadata for the relation between a class and its parents */
         if(Decl->getNumBases()){
+          if(Decl->getNumBases() > 1){
+            ::fprintf(stderr, "Attention, we do not support multiple bases yet");
+          }
           for(CXXBaseSpecifier Base : Decl->bases()){
             llvm::Type *ParentTyp = getTypes().ConvertType(Base.getType());
 
